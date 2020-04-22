462c18e163591cf511cfbdd76d7f975c31a5251c
daiyu <jiangyufeng.0918@gmail.com>
2020-04-22 15:29:43 +0800

    nacos integration

--- gulimall-coupon/src/main/java/com/bp/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/bp/gulimall/coupon/GulimallCouponApplication.java
@@ -2,7 +2,9 @@ package com.bp.gulimall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallCouponApplication {
 

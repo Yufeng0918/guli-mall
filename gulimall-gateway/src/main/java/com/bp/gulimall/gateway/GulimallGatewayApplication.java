947d188a56242c89b7552469c798b6cfd5745c46
daiyu <jiangyufeng.0918@gmail.com>
2020-05-02 10:23:35 +0800

    gateway CORS configuration

--- gulimall-gateway/src/main/java/com/bp/gulimall/gateway/GulimallGatewayApplication.java
+++ gulimall-gateway/src/main/java/com/bp/gulimall/gateway/GulimallGatewayApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-//@SpringBootApplication
 public class GulimallGatewayApplication {
 
     public static void main(String[] args) {

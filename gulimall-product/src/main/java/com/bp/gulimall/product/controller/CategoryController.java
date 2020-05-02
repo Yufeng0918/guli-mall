506790865df9ca555ade3209d6732bd802eb561f
daiyu <jiangyufeng.0918@gmail.com>
2020-05-02 10:32:51 +0800

    product register to nacos as service discovery server and configure server

--- gulimall-product/src/main/java/com/bp/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/bp/gulimall/product/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.bp.gulimall.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
@@ -34,11 +35,11 @@ public class CategoryController {
     /**
      * 列表
      */
-    @RequestMapping("/list")
+    @RequestMapping("/list/tree")
     //@RequiresPermissions("product:category:list")
     public R list(@RequestParam Map<String, Object> params){
         PageUtils page = categoryService.queryPage(params);
-
+        List<CategoryEntity> entities = categoryService.listWithTree();
         return R.ok().put("page", page);
     }
 
--- gulimall-product/src/main/java/com/bp/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/bp/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package com.bp.gulimall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +29,30 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+        List<CategoryEntity> level1Menus = entities.stream().filter(categoryEntity ->categoryEntity.getParentCid() == 0).map((menu)->{
+            menu.setChildren(getChildrens(menu,entities));
+            return menu;
+        }).sorted(Comparator.comparingInt(menu -> (menu.getSort() == null? 0 : menu.getSort()))).collect(Collectors.toList());
+        return level1Menus;
+    }
+
+
+    private List<CategoryEntity> getChildrens(CategoryEntity root,List<CategoryEntity> all){
+
+        List<CategoryEntity> children = all.stream().filter(categoryEntity -> {
+            return categoryEntity.getParentCid() == root.getCatId();
+        }).map(categoryEntity -> {
+            categoryEntity.setChildren(getChildrens(categoryEntity,all));
+            return categoryEntity;
+        }).sorted(Comparator.comparingInt(menu -> (menu.getSort() == null? 0 : menu.getSort()))).collect(Collectors.toList());
+
+        return children;
     }
 
 }
